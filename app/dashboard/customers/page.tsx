9a6985adf15f47bd6b9373056d1e235f5999ae6d
Brian Hood <hoodbri@gmail.com>
2025-07-11 18:18:45 -0500

    Adding customer table to customer page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,24 +1,26 @@
-// import Table from "@/app/ui/customers/table"
-// import Search from "@/app/ui/search";
+import Table from "@/app/ui/customers/table"
 import { Metadata } from "next";
-// import { Suspense } from "react";
+import { Suspense } from "react";
+import { CustomersTableSkeleton } from "@/app/ui/skeletons";
+import { fetchFilteredCustomers } from "@/app/lib/data";
 
 export const metadata: Metadata = {
 	title: 'Customers',
 };
 
-export default function Page() {
-	return <p>Customer Page</p>;
+export default async function Page(props: {
+	searchParams?: Promise<{
+		query?: string;
+		page?: string;
+	}>;
+}) {
+	const searchParams = await props.searchParams;
+	const query = searchParams?.query || '';
+	const customers = await fetchFilteredCustomers(query);
+
+	return (
+		<Suspense key={query} fallback={<CustomersTableSkeleton />}>
+				<Table customers={customers} />
+		</Suspense>
+	);
 }
-
-// export default async function Page(props: {
-// 	searchParams?: Promise<{
-// 		query?: string;
-// 		page?: string;
-// 	}>;
-// }) {
-// 	const searchParams = await props.searchParams;
-// 	const query = searchParams?.query || '';
-
-// 	return <p>Customer Page</p>;
-// }
